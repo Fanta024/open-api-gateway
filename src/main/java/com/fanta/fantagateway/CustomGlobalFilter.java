f6d45bd6349f1de8a39ad73256ee188b35ca49de
Fanta <857035389@qq.com>
2023-06-04 15:55:56 +0800

    添加全局过滤器

--- src/main/java/com/fanta/fantagateway/CustomGlobalFilter.java
+++ src/main/java/com/fanta/fantagateway/CustomGlobalFilter.java
@@ -1,18 +1,27 @@
 package com.fanta.fantagateway;
 
 
+import com.fanta.fantaclientsdk.utils.SignUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpResponseDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,17 +38,103 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         ServerHttpResponse response = exchange.getResponse();
         String address = request.getRemoteAddress().getHostString();
         System.out.println(address);
-        if (IP_WHITE_LIST.contains(address)) {
+        //对非白名单地址的请求拦截
+        if (!IP_WHITE_LIST.contains(address)) {
             response.setStatusCode(HttpStatus.FORBIDDEN);
             return response.setComplete();
         }
-        return chain.filter(exchange);
+        HttpHeaders headers = request.getHeaders();
+        String accessKey = headers.getFirst("accessKey");
+        String sign = headers.getFirst("sign");
+        String body = headers.getFirst("body");
+        String nonce = headers.getFirst("nonce");
+        String timestamp = headers.getFirst("timestamp");
+        final long FIVE_TIME = 5 * 60L;
+        if ((System.currentTimeMillis() / 1000) - Long.parseLong(timestamp) >= FIVE_TIME) {
+            response.setStatusCode(HttpStatus.FORBIDDEN);
+            return response.setComplete();
+        }
+
+        //判断接口是否存在
+
+        //ak、sk是否合法
+        //实际情况是从数据库中查取sk
+        String serverSign = SignUtils.getSign(body, "asdqwe");
+        if (!serverSign.equals(sign)) {
+            return handleNoAuth(response);
+        }
+        //请求转发 调用接口请求次数+1  调用之前写过的接口    invokeCount
+        return handleResponse(exchange, chain);
+//        //调用成功
+//        response.setStatusCode(HttpStatus.OK);
+//        //调用失败
+//        if (response.getStatusCode() != HttpStatus.OK) {
+//            return handleInvokeError(response);
+//        }
     }
 
     @Override
     public int getOrder() {
         return -1;
     }
+
+    public Mono<Void> handleNoAuth(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        return response.setComplete();
+    }
+
+    public Mono<Void> handleInvokeError(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+        return response.setComplete();
+    }
+
+    public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain) {
+        try {
+            ServerHttpResponse originalResponse = exchange.getResponse();
+            DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+
+            HttpStatus statusCode = originalResponse.getStatusCode();
+
+            if (statusCode == HttpStatus.OK) {
+
+                ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
+
+                    @Override
+                    public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                        //log.info("body instanceof Flux: {}", (body instanceof Flux));
+                        if (body instanceof Flux) {
+                            Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                            return super.writeWith(fluxBody.map(dataBuffer -> {
+                                byte[] content = new byte[dataBuffer.readableByteCount()];
+                                dataBuffer.read(content);
+                                DataBufferUtils.release(dataBuffer);//释放掉内存
+                                // 构建日志
+                                StringBuilder sb2 = new StringBuilder(200);
+                                sb2.append("<--- {} {} \n");
+                                List<Object> rspArgs = new ArrayList<>();
+                                rspArgs.add(originalResponse.getStatusCode());
+                                //rspArgs.add(requestUrl);
+                                String data = new String(content, StandardCharsets.UTF_8);//data
+                                sb2.append(data);
+                                log.info("响应结果:{}", data);
+                                log.info("array:{}", rspArgs.toArray());
+                                return bufferFactory.wrap(content);
+                            }));
+                        } else {
+                            log.error("<--- {} 网关处理响应异常", getStatusCode());
+                        }
+                        return super.writeWith(body);
+                    }
+                };
+                return chain.filter(exchange.mutate().response(decoratedResponse).build());
+            }
+            return chain.filter(exchange);//降级处理返回数据
+        } catch (Exception e) {
+            log.error("gateway log exception.\n" + e);
+            return chain.filter(exchange);
+        }
+    }
+
 }
 
 
