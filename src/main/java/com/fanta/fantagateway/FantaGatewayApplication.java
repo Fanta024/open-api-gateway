e41a8f045a59c2c5a11bbc86b4aec7e78b1790ff
Fanta <857035389@qq.com>
2023-06-12 20:57:13 +0800

    添加Dubbo

--- src/main/java/com/fanta/fantagateway/FantaGatewayApplication.java
+++ src/main/java/com/fanta/fantagateway/FantaGatewayApplication.java
@@ -1,9 +1,11 @@
 package com.fanta.fantagateway;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class FantaGatewayApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/fanta/springbootinit/provider/DemoService.java
@@ -0,0 +1,5 @@
+package com.fanta.springbootinit.provider;
+
+public interface DemoService {
+    String sayHello(String name);
+}
